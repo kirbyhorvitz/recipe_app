24a150581403f72bc962c1b16b3b7a7ff01a87e9
kirbyhorvitz <kth49@georgetown.edu>
2021-05-13 17:25:22 -0400

    Add our recipe page and create cards with data file loop

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.use(flash())
 
 app.use('/', indexRouter);
 
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-<!---Camden Gilliam helped set up API route to Index home page--->
+//Camden Gilliam helped set up API route to Index home page
 var fetch = require('node-fetch');
 var express = require('express');
 var router = express.Router();
+var ourRecipes = require('../data/ourRecipes.json')
 
 const API_KEY = process.env.RECIPES_API_KEY || "abc123" // obtain your own api key and set via environment variable
 
@@ -10,6 +11,26 @@ router.get('/about', function(req, res, next) {
   res.render('about');
 });
 
+/* GET our recipes page. */
+router.get('/ourrecipes', function(req, res, next) {
+  res.render('ourrecipes', {data: ourRecipes});
+});
+
+
+router.get('/ourrecipes/:id', function(req, res, next) {
+ //recipe id is in req.params.id
+
+ let data = null;
+ for (var i=0; i < ourRecipes.length; i++ ) {
+   if (ourRecipes[i].id == req.params.id) {
+     data = ourRecipes[i]
+   }
+ }
+
+ res.render('recipe', {data:data});
+
+
+})
 
 /* GET recipes. */
 
@@ -23,7 +44,7 @@ router.get('/recipelist', function(req, res, next) {
         return response.json()
     })
     .then(function(data){
-      console.log("Data", data)
+          console.log("Data", data)
 
            res.render("recipes", {data:data});
       })
@@ -44,8 +65,14 @@ router.get('/about', (req, res) => {
 	res.render('about', { user: req.user });
 });
 
+router.get('/ourrecipes', (req, res) => {
+	res.render('ourrecipes', { user: req.user });
+});
+
 router.get('/recipelist', (req, res) => {
 	res.render('recipelist', { user: req.user });
 });
 
 module.exports = router;
+
+
